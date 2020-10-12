cdc85c8ea4107146c2bd923a8f6575c60cc1eecb
Matyáš Brabec <matyas.brabec@profinit.eu>
2020-10-12 11:17:33 +0200

    added turnamentor CLI

---
+++ Turnamentor.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Turnamentor.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
